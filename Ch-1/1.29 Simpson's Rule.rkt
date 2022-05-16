96ef731bc5c6a5c493d7bcb8792626d92b18c35e
sivathanuk <62845445+sivathanuk@users.noreply.github.com>
2022-05-16 23:41:08 +0530

    Create 1.29 Simpson's Rule.rkt

---
+++ Ch-1/1.29 Simpson's Rule.rkt
@@ -0,0 +1,22 @@
+; simpson's rule
+
+(define (sum term a next b)
+  (if (> a b) 0
+      (+ (term a) (sum term (next a) next b))))
+
+(define (simpsons fun a b n)
+  
+  (define h (/ (- b a) n))
+
+  (define (coeff k)
+    (cond ((or (= 0 k)
+               (= n k)) 1)
+          ((is-even k) 2)
+          (else 4)))
+
+  (define (yk k)
+    (* (coeff k)
+       (fun (+ a
+               (* k h)))))
+  (* (sum yk a inc n)
+     ( / h 3.0)))
