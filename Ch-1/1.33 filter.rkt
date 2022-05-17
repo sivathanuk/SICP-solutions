94c6e7a2dccbe563416343bd11ad243f10194c22
sivathanuk <62845445+sivathanuk@users.noreply.github.com>
2022-05-17 23:07:55 +0530

    Update 1.33 filter.rkt

--- Ch-1/1.33 filter.rkt
+++ Ch-1/1.33 filter.rkt
@@ -1,25 +1,56 @@
 #lang sicp
 
-; filter 
-
-(define (accumulate combiner null-value term a next b)
-  (if (> a b) null-value
-      (combiner (term a) (accumulate combiner null-value term (next a) next b))))
-
-
-(define (filtered-accumulate filter combiner null-value term a next b)
+; filtered-accumulate
+(define (filtered-accumulate filter null-value combiner term a next b)
   (if (> a b)
       null-value
       (if (filter a)
-          (combiner (term a) (filtered-accumulate filter combiner null-value term (next a) next b))
-          (combiner null-value (filtered-accumulate filter combiner null-value term (next a) next b)))))
+          (combiner (term a) (filtered-accumulate filter null-value combiner term (next a) next b))
+          (combiner null-value (filtered-accumulate filter null-value combiner term (next a) next b)))))
 
+; sq
+(define (sq n)
+  (* n n))
 
+; is-even
 (define (is-even n)
   (= (remainder n 2) 0))
 
-(define (cube n)
-  (* n n n))
+; is-prime
+(define (is-prime p)
+  (= (find-smallest-divisor p 2) p))
 
-; even-filter
-(filtered-accumulate is-even + 0 cube 1 inc 9)
+
+(define (find-smallest-divisor p n)
+  (cond ((< p (sq n)) p)
+        ((is-divisible p n) n)
+        (else
+         (find-smallest-divisor p (+ n 1)))))
+
+(define (is-divisible p n)
+  (= (remainder p n) 0))
+
+; gcd
+(define (gcd n d)
+  (if (= (remainder n d) 0)
+      d
+      (gcd d (remainder n d))))
+
+; identity
+(define (identity x)
+  x)
+; a-prime addition
+
+(filtered-accumulate is-prime 0 + sq 2 inc 5)
+
+
+; b-relative-prime multiplication
+(define (rel-prime-mul p)
+  (define (rel-prime n)
+    (= (gcd p n) 1))
+  (filtered-accumulate rel-prime 1 * identity 1 inc p))
+
+
+
+
+      
