fa9ba8156ddf261c6ed6e81b5d32ca91cd09f7bb
sivathanuk <62845445+sivathanuk@users.noreply.github.com>
2022-06-07 00:01:26 +0530

    Added iterative solution

--- Ch-1/1.43 Repeat function.rkt
+++ Ch-1/1.43 Repeat function.rkt
@@ -1,8 +1,18 @@
 #lang sicp
 
-; repeated - recursion
+; repeat function - recursion
 
 (define (repeat f n)
   (if (= n 1)
       f
       (compose f (repeat f (- n 1)))))
+      
+
+; repeat fucntion - iteration
+
+(define (repeat-i f n)
+  (define (repeat-iter result k)
+    (if (= k 1)
+        result
+        (repeat-iter (compose result f) (- k 1))))
+  (repeat-iter f n))
