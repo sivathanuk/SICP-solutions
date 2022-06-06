de57f7602d87c3f488e3916f6e6c734e612eb1b5
sivathanuk <62845445+sivathanuk@users.noreply.github.com>
2022-06-06 23:49:18 +0530

    Update 1.43 Repeat function.rkt

--- Ch-1/1.43 Repeat function.rkt
+++ Ch-1/1.43 Repeat function.rkt
@@ -1,26 +1,8 @@
 #lang sicp
 
-(define (compose f g)
-  (lambda (x) (f (g x))))
+; repeated - recursion
 
-(define (repeated f n)
-    (if (= n 1)
-        f
-        (repeated (compose f f) (- n 1))))
-
-
-(define (square n)
-  (* n n))
-
-; check
-((repeated square 2) 5)
-((repeated cos 25) 0.739)
-((repeated inc 5) 5)
-
-#|Output:
-
-625
-0.7390851332151605
-21
-
-|#
+(define (repeat f n)
+  (if (= n 1)
+      f
+      (compose f (repeat f (- n 1)))))
