36a75b6a6668540d6d6ff5bdcefab2506a644b8b
sivathanuk <62845445+sivathanuk@users.noreply.github.com>
2022-06-07 21:52:40 +0530

    Create Chapter 1.rkt

---
+++ Pratice/Chapter 1.rkt
@@ -0,0 +1,23 @@
+#lang sicp
+
+; exp r,i
+; sum procedure r,i
+; primality divisor
+; fibonacci r,i
+; sqrt normal
+; half-interval method
+; GCD
+; primality Euler
+; integral cube- [f(a + dx/2) + f(a + dx + dx/2) + f(a + 2dx + dx/2)+...] * dx
+; Simpsons Method cube
+#| h/3 [y0 +  4 * y1 + 2 * y2 + 4 * y3 + 2 * y4 + ....+ 2 * y(n-2) + 4 * y(n-1) + yn]
+h = (b-a)/ n 
+yk = f(a + kh)|#
+; cont-frac 1 r,i
+; pi/4- (2 * 4 * 4 * 6 * 6 * 8....) / (3 * 3 * 5 * 5 * 7 * 7....)
+; fixed-point
+; sqrt average damp
+; sqrt Newton's
+; repeat function
+; repeat fixed-point average damp to find roots
+; iterative improvement - sqrt. fixed-point
