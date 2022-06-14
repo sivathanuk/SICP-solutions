003c589cef0bc12078bd0567206a71b23bdd0d6b
sivathanuk <62845445+sivathanuk@users.noreply.github.com>
2022-06-14 18:23:36 +0530

    Update 2.1 signed make-rat.rkt

--- chapter 2/2.1 signed make-rat.rkt
+++ chapter 2/2.1 signed make-rat.rkt
@@ -1,20 +1,23 @@
 #lang sicp
-
+; helper functions
 (define (gcd n d)
   (if (= (remainder n d) 0)
       (abs d)
       (gcd d (remainder n d))))
+
+(define (sign a)
+    (if (< a 0)
+        -1
+        1))
 
 ;(gcd 21 35)
 
 (define (Make-rat x y)
   (let
       ((g (gcd x y)))
-    (if (or
-         (and (negative? x) (positive? y))
-         (and (positive? x) (negative? y)))
-        (cons (- (/ (abs x) g)) (/ (abs y) g))
-        (cons (/ (abs x) g) (/ (abs y) g)))))
+    (cons (* (sign x) (sign y) (abs (/ x g)))
+          (abs (/ y g)))
+    ))
 
 (define (print-rat z)
   (newline)
@@ -22,14 +25,16 @@
   (display "/")
   (display (cdr z)))
 
+
 (print-rat (Make-rat -3 4))
 (print-rat (Make-rat 3 -4))
 (print-rat (Make-rat -3 -4))
 (print-rat (Make-rat 3 4))
-
+(newline)
 (print-rat (Make-rat -40 4))
 (print-rat (Make-rat 1250 -25))
 (print-rat (Make-rat 5 25))
+
 
 
 #| output:
@@ -38,7 +43,6 @@
 -3/4
 3/4
 3/4
-
 -10/1
 -50/1
 1/5
