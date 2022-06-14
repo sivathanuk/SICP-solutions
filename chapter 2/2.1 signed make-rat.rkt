3e97db284edf3da2e18cd8b2a47f71f22657c9b4
sivathanuk <62845445+sivathanuk@users.noreply.github.com>
2022-06-14 18:13:34 +0530

    Update 2.1 signed make-rat.rkt

--- chapter 2/2.1 signed make-rat.rkt
+++ chapter 2/2.1 signed make-rat.rkt
@@ -1,4 +1,3 @@
-
 #lang sicp
 
 (define (gcd n d)
@@ -14,8 +13,8 @@
     (if (or
          (and (negative? x) (positive? y))
          (and (positive? x) (negative? y)))
-        (cons (- (abs x)) (abs y))
-        (cons (abs x) (abs y)))))
+        (cons (- (/ (abs x) g)) (/ (abs y) g))
+        (cons (/ (abs x) g) (/ (abs y) g)))))
 
 (define (print-rat z)
   (newline)
@@ -28,9 +27,19 @@
 (print-rat (Make-rat -3 -4))
 (print-rat (Make-rat 3 4))
 
+(print-rat (Make-rat -40 4))
+(print-rat (Make-rat 1250 -25))
+(print-rat (Make-rat 5 25))
+
 
 #| output:
+
 -3/4
 -3/4
 3/4
-3/4 |#
+3/4
+-10/1
+-50/1
+1/5
+
+|#
